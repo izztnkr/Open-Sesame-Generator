3111e103e8b0c8e9a5d48acda7f627d4da30f26d
Ethan Chandler <86173119+IzzTinkr@users.noreply.github.com>
2021-08-29 15:07:24 -0500

    prompt boxes for questions are done, sequence is correct

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,82 +1,69 @@
 //create a button
 //link button to js via id
 var buttonEl = document.querySelector("#generate");
-var passwordChoices = ['character length', 'character types' ]
-
-
-//create eventlistener on click of button
-buttonEl.addEventListener("click", ) 
-  Symbols;
-}
-function Symbols() {
-  var txt;
-  if (confirm, "Include Symbols?"){
-    txt = "Ok! Symbols included.";
-  }
-  else{
-    txt = "OK! Symbols NOT included.";
-  }
-};
-
-// {
-
-//   // userTurn = confirm('Include Symbols?');
-//   //   if (confirm, "Goood") {
-//   //   userTurn = confirm('true?'); 
-//   //   if (true) {
-//   //     userTurn = confirm('Include Uppercase?');
-//   //   }
-//   // }
-//   // else, if (false){
-//   //   userTurn = confirm('false');
-//   //   if (true) {
-//   //     userTurn = confirm('Include Uppercase?');
-//   //   }  
-//   // }
-  
-// });
-
-// function Symbols() {
-//   var txt;
-//   if (confirm ( "Include Symbols?"){
-//     txt = "Ok! Symbols included.";
-//   }
-//   else{
-//     txt = "OK! Symbols NOT included.";
-//   }
-// };
 
 
 
+//create eventlistener on click of button (// "WHEN I click the button to generate a password")
+buttonEl.addEventListener ("click", criteriaFunction) 
+ 
 
 
-// create a series of prompts for password criteria,
-//display via alert
-//triggger on click which means call this function in the click event listenr
-
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
-
-}
-
-
-
-
-// "WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria"
-// "WHEN prompted for password criteria
-// THEN I select which criteria to include in the password"
-// "WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters"
-// "WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters"
+// create a series of prompts for password criteria,   
+//display via alert (// THEN I am presented with a series of prompts for password criteria)
+//(// "WHEN prompted for password criteria
+// THEN I select which criteria to include in the password")(// "WHEN asked for character types to include in the password
+// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters") // "WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
 // "WHEN I answer each prompt
 // THEN my input should be validated and at least one character type should be selected"
-// "WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria"
-// "WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page"
+
+function criteriaFunction(userTurn){
+   userTurn1 = confirm('Include Symbols?');
+    if (userTurn1 === true) { 
+      alert ("Ok! Symbols will be included.")
+    } else { 
+      alert("Ok! Symbols will NOT be included.")
+    };
+   userTurn2 = confirm('Include Uppercase letters?');
+    if (userTurn2 === true) { 
+      alert("Ok! Uppercase letters will be included.");
+    } else {
+      alert("Ok! Uppercase letters will NOT be included.");
+    };
+   userTurn3 = confirm('Include Lowercase letters?');
+    if (userTurn3 === true) {
+      alert("Ok! Lowercase letters will be included.");
+    } else {
+      alert("Ok! Lowercase letters will NOT be included.");
+    };
+    //will need to use the prompt() Method here
+   userTurn4 = prompt("Please choose a character length between 8 and 128", "");
+    if (userTurn4 != null){
+      document.getSelection()
+      alert("Ok! " + inputValue + " will be used.");
+    } else {
+      userTurn4;
+    }
+   //figure out a way to impede user from entering anything but numbers for this box.
+}
+
+  
+ 
+//What to do with the confirm or cancel? Use as parameters for the password generator. How?
+// Write password to the #password box in the body of the HTML (// "WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page")) (// "WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria")
+function writePassword() {
+   var password = generatePassword();
+   var passwordText = document.querySelector("#password");
+   passwordText.value = password;
+
+ }
+
+
+
+
+
+
+
