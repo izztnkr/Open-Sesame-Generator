0e2717a51e7ad8232a82b49afe3cc02fd32ad9bc
Ethan Chandler <86173119+IzzTinkr@users.noreply.github.com>
2021-09-03 20:18:01 -0500

    cleaned funtions, found a more efficient way to write some variables, fixed generating issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,92 +1,88 @@
 //create a button
 //link button to js via id
 var buttonEl = document.querySelector("#generate");
-var UpperCase_CHAR_CODES = arrayFromLowtoHigh(65,90)
-var lowerCase_CHAR_CODES = arrayFromLowtoHigh(97,122)
-var Numbers_CHAR_CODES = arrayFromLowtoHigh(48,57)
-var Symbol_CHAR_CODES = arrayFromLowtoHigh(33,47).concat(arrayFromLowtoHigh(58.64)).concat(arrayFromLowtoHigh(91,96)).concat(arrayFromLowtoHigh(123, 126))
+var UpperCase_CHAR_CODES = arrayFromLowtoHigh(65, 90)
+var lowerCase_CHAR_CODES = arrayFromLowtoHigh(97, 122)
+var Numbers_CHAR_CODES = arrayFromLowtoHigh(48, 57)
+var Symbol_CHAR_CODES = arrayFromLowtoHigh(33, 47).concat(arrayFromLowtoHigh(58.64)).concat(arrayFromLowtoHigh(91, 96)).concat(arrayFromLowtoHigh(123, 126))
+var includeSymbols = false;
+var includeNumbers = false;
+var includeUppercase = false;
+var includeLowercase = false;
 
+buttonEl.addEventListener("click", criteriaFunction)
 
-buttonEl.addEventListener ("click", criteriaFunction) 
+function criteriaFunction() {
+  includeSymbols = confirm('Include Symbols?');
+  if (includeSymbols === true) {
+    alert("Ok! Symbols will be included.")
+  } else {
+    alert("Ok! Symbols will NOT be included.")
+  };
+  includeNumbers = confirm('Include Numbers?');
+  if (includeNumbers === true) {
+    alert("Ok! Numbers will be included.")
+  } else {
+    alert("Ok! Numbers will NOT be included.")
+  };
+  includeUppercase = confirm('Include Uppercase letters?');
+  if (includeUppercase === true) {
+    alert("Ok! Uppercase letters will be included.");
+  } else {
+    alert("Ok! Uppercase letters will NOT be included.");
+  };
+  includeLowercase = confirm('Include Lowercase letters?');
+  if (includeLowercase === true) {
+    alert("Ok! Lowercase letters will be included.");
+  } else {
+    alert("Ok! Lowercase letters will NOT be included.");
+  };
+  passwordLength = prompt("Please choose a character length between 8 and 128", "");
+  inputCharacterLength(passwordLength);
+}
 
-
-function criteriaFunction(userTurn){
-   var includeSymbols = confirm('Include Symbols?');
-    if (includeSymbols === true) { 
-      alert ("Ok! Symbols will be included.")
-    } else { 
-      alert("Ok! Symbols will NOT be included.")
-    };
-    var includeNumbers = confirm('Include Numbers?');
-    if (includeNumbers === true) { 
-      alert ("Ok! Numbers will be included.")
-    } else { 
-      alert("Ok! Numbers will NOT be included.")
-    };
-    var includeUppercase = confirm('Include Uppercase letters?');
-    if (includeUppercase === true) { 
-      alert("Ok! Uppercase letters will be included.");
+function inputCharacterLength(passwordLength) {
+  if (passwordLength != null) {
+    var generate = confirm("Ok! " + passwordLength + " characters will be used.");
+    if (generate === true) {
+      writePassword(passwordLength);
     } else {
-      alert("Ok! Uppercase letters will NOT be included.");
-    };
-   var includeLowercase = confirm('Include Lowercase letters?');
-    if (includeLowercase === true) {
-      alert("Ok! Lowercase letters will be included.");
-    } else {
-      alert("Ok! Lowercase letters will NOT be included.");
-    };
-    
-  
-   var passwordLength = prompt("Please choose a character length between 8 and 128", "");
-   inputCharacterLength(passwordLength);
-
-   function inputCharacterLength(passwordLength){
-    if (passwordLength != null){
-      var generate = confirm("Ok! " + passwordLength + " characters will be used.");
-        if (generate === true){
-          writePassword(generatePassword);
-        
-        }else{
-          alert("Password generation cancelled.")
-        }
-    } else {
-      alert("Invalid Choice!!\n Please choose a number between 8 and 128");
-      var passwordLength = prompt("Please choose a character length between 8 and 128", "");
-      inputCharacterLength(passwordLength)
+      alert("Password generation cancelled.")
     }
-    
+  } else {
+    alert("Invalid Choice!!\n Please choose a number between 8 and 128");
+    var passwordLength = prompt("Please choose a character length between 8 and 128", "");
+    inputCharacterLength(passwordLength)
   }
-  
-  function generatePassword(includeSymbols, includeNumbers, includeUppercase, includeLowercase){
-    let charCodes = lowerCase_CHAR_CODES
-    if (includeUppercase) charCodes =charCodes.concat(UpperCase_CHAR_CODES)
-    if (includeSymbols) charCodes= charCodes.concat(Symbol_CHAR_CODES)
-    if (includeNumbers) charCodes= charCodes.concat(Numbers_CHAR_CODES)
+}
 
-     var passwordCombo = []
-     for (let i = 0; i <passwordLength; i++){
-      var characterCodes = charCodes[Math.floor(Math.random()*passwordLength)]
-        passwordCombo.push(String.fromCharCode(charCodes))
-    }
-     return passwordCombo.join('')
+function writePassword(passwordLength) {
+  var password = generatePassword(passwordLength);
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+function generatePassword(passwordLength) {
+  let charCodes = lowerCase_CHAR_CODES
+  if (includeUppercase) charCodes = charCodes.concat(UpperCase_CHAR_CODES)
+  if (includeSymbols) charCodes = charCodes.concat(Symbol_CHAR_CODES)
+  if (includeNumbers) charCodes = charCodes.concat(Numbers_CHAR_CODES)
+  console.log('CHAR CODES', charCodes)
+  var passwordCombo = []
+  for (let i = 0; i < passwordLength; i++) {
+    var characterCodes = charCodes[Math.floor(Math.random() * 100)]
+    console.log('CHARACTERCODES', characterCodes);
+    passwordCombo.push(String.fromCharCode(characterCodes))
   }
-
-
-  function writePassword(includeSymbols, includeNumbers, includeUppercase, includeLowercase) {
-    var password = generatePassword(includeSymbols, includeNumbers, includeUppercase, includeLowercase);
-    var passwordText = document.querySelector("#password");
-    passwordText.value = password;
-  } 
- 
-   
+  return passwordCombo.join('')
 }
 
 //using this in stead of writing out a list of character codes
-function arrayFromLowtoHigh(low,high) {
+function arrayFromLowtoHigh(low, high) {
   var array = []
-  for (let i = low; i <= high; i++){
+  for (let i = low; i <= high; i++) {
     array.push(i)
   }
   return array
 }
- 
+
